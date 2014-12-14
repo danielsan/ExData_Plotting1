0bdc24bac5c6c1cdcefa36cfd2f4f12033f0ba90
Daniel Santana <daniel@everymundo.com>
2014-12-14 18:07:48 -0500

    plot1.R and .plot1.png

---
+++ plot1.R
@@ -0,0 +1,9 @@
+source('get_data.R')
+source('my_png_plot.R')
+
+my_png_plot('plot1', function() hist(
+  get_data()$Global_active_power,
+  col  = "red",
+  xlab = "Global Active Power (kilowatts)", 
+  main = "Global Active Power"))
+
