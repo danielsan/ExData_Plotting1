5085d3486a772eaa9d55d70d01cd99c37c9e45f7
Daniel Santana <daniel@everymundo.com>
2015-01-10 20:36:03 -0500

    Refactored my_png_plot function

--- my_png_plot.R
+++ my_png_plot.R
@@ -1,5 +1,5 @@
-my_png_plot <- function(png_name, func) {
-  png(paste(png_name, '.png', sep=''), width=480, height=480)
+my_png_plot <- function(png_file_name, func) {
+  png(paste(png_file_name, '.png', sep = ''), width = 480, height = 480)
   func()
   dev.off()
 }
