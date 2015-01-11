aef56d4404bed607f3c7450d6e007b4715b4e057
Daniel Santana <daniel@everymundo.com>
2015-01-10 20:38:01 -0500

    Refactoring of plot2.R file

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 source('get_data.R')
 source('my_png_plot.R')
 data <- get_data()
-data <- strptime(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+data$DateTime <- strptime(paste(data$Date, data$Time), format = '%d/%m/%Y %H:%M:%S')
 
-my_png_plot('plot2', function() plot(data,Data$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)", type="l"))
+my_png_plot('plot2', function() plot(data$DateTime, data$Global_active_power, type='l', ylab = 'Global Active Power (kilowatts)', xlab = ''))
