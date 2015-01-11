2c6125142b70038158e10534511ac5c478f0ef56
Daniel Santana <daniel@everymundo.com>
2015-01-10 20:36:31 -0500

    get_date function reformatted

--- get_data.R
+++ get_data.R
@@ -1,13 +1,13 @@
 get_data <- function () {
   if(!require('sqldf')) {
-    install.packages("sqldf")
+    install.packages('sqldf')
     require('sqldf')
   }
 
   inner_data <- function() {
     if(!exists('get_data.data')) {
       query <- "SELECT * from file WHERE Date IN('1/2/2007', '2/2/2007')"
-      get_data.data <<- read.csv.sql("./household_power_consumption.txt",sql=query, sep=";")
+      get_data.data <<- read.csv.sql("./household_power_consumption.txt", sql = query, sep=";")
     }
     get_data.data
   }
